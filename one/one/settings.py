95ba382d2cefc79ad48c649c158df9e0e5dcb9f8
Issa <g.lectricals@gmail.com>
2026-01-08 19:08:32 +0300

    this is the third day

--- one/one/settings.py
+++ one/one/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8=4&6!8bnkdy-fe-6yo_@cgvz#p(a%+n_ka+jo(7l8*jtku(qi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'one.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
